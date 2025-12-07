5d2a7c4c40ee22b3ed465cf59e93e23c08fb8f93
Chaerim Han <96909637+cozyrim@users.noreply.github.com>
2025-12-07 14:45:08 +0900

    fix JwtAuthenticationFilter shouldNotFilter add

--- src/main/java/com/ktbweek4/community/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/ktbweek4/community/auth/jwt/JwtAuthenticationFilter.java
@@ -44,7 +44,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-
+        System.out.println("🚨 doFilterInternal — 요청 URI: " + request.getRequestURI());
         String token = resolveToken(request);
 
         if (token != null && jwtTokenProvider.validateToken(token)) {
